be3d70c69619d2dd81d7428ee68402ce84fb26fc
Anush Indrajith <anushcs55@gmail.com>
2018-10-05 11:56:27 +0530

    install cpu stat for cpu core details

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const index = express();
 const port = process.env.port || 3000 ;
 const system = require('./routes/system');
+
+index.use(bodyParser.urlencoded({ extended: false }));
+index.use(bodyParser.json());
 
 index.use(express.static(path.join(__dirname,"public")));
 
@@ -15,3 +19,19 @@ index.get("/",function(req,res){
 index.listen(port, function (){
       console.log("listen to port " + port);
 });
+var cpuStats = require('cpu-stats');
+ 
+// the first argument is how long to sample for in ms.
+// longer is more accurate but, you know, longer.
+// if omitted, defaults to one second.
+
+
+
+setInterval(function() {
+    cpuStats(1000, function(error, result) {
+        if(error) return console.error('Oh noes!', error) // actually this will never happen
+       
+        console.info(result[1].cpu);
+      });
+
+  }, 1000); 
